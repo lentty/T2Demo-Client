2e3eadccd90a452cfef5198b232590b43d2bb4ae
huangmin <minlentty@gmail.com>
2018-08-05 17:26:02 +0800

    add home tab

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 //app.js
 App({
+  data: {
+    appid: 'wxb0f25ab0f0fe1a2f',
+    appsecret: 'df5cd9d92cd000596b1ba7e76c363fce',
+    api: 'https://api.weixin.qq.com/sns/jscode2session?'
+  },
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
+   var that = this
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
+        if(res.code){
+          var d =  that.data;
+          var url = d.api+ 'appid=' + d.appid + '&secret=' + d.appsecret + '&js_code=' + res.code + '&grant_type=authorization_code';
+          console.log('url: '+url);
+          wx.request({
+            url: url,
+            method: 'GET',
             success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+              console.log(res.data);   
+              if(res.data.openid){
+                wx.setStorageSync('openid', res.data.openid); 
+              }   
             }
           })
         }
