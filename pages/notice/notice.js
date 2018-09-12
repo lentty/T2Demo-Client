bcaf29a9c83c8a869a873fb223b1f5a5262104b6
smallsun <yanglingz610@sina.com>
2018-09-12 23:42:04 +0800

    init announcement page

---
+++ pages/notice/notice.js
@@ -0,0 +1,81 @@
+// pages/notice/notice.js
+import Util from '../../utils/util';
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    notice: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let that = this;
+    wx.request({
+      url: app.globalData.host + '/announcement/list',
+      method: 'GET',
+      success: function (res) {
+        if (res.data.msg === 'ok') {
+          that.setData({ notice: res.data.retObj[0]})
+        }
+      },
+      fail: function (error) {
+        console.log(error);
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
