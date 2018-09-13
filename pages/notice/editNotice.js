47e3cc1ecbe8df4ae9b538231f8f78b56293bc7d
Lingzhen <xxx.com>
2018-09-13 19:15:25 +0800

    add edit announcement page

--- pages/notice/editNotice.js
+++ pages/notice/editNotice.js
@@ -1,27 +1,28 @@
-// pages/notice/notice.js
-import Util from '../../utils/util';
-const app = getApp()
-
+// pages/notice/editNotice.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    notice: {}
+    content: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let that = this;
+    this.setData({content: options.noticeCont});
+  },
+
+  saveNotice: function (evt) {
+    let content = evt.detail.value.noticeCont;
     wx.request({
-      url: app.globalData.host + '/announcement/list',
-      method: 'GET',
+      url: '',
+      method: 'POST',
       success: function (res) {
         if (res.data.msg === 'ok') {
-          that.setData({ notice: res.data.retObj[0]})
+          Util.showToast('保存成功', 'success', 2000);
         }
       },
       fail: function (error) {
@@ -30,64 +31,52 @@ Page({
     })
   },
 
-  editAnnouncement: function () {
-    let userInfo = wx.getStorageSync('userInfo');
-    if (userInfo.status === 2) {
-      wx.navigateTo({
-        url: 'editNotice?noticeCont=' + this.data.notice.content,
-      })
-    }
-    wx.navigateTo({
-      url: 'editNotice?noticeCont=' + this.data.notice.content,
-    });
-  },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+  
   }
 })
